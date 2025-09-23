f4072c4c8e2c0ea67163da2069f354b4d23a67cf
Anvesh Trivedi <trivedianvesh07@gmail.com>
2025-09-23 18:53:07 +0000

    data types

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version.
+
+// alert(3+3) we are using nodejs , not browser so it will throw an error 
+console.log(3+3);
+ let name = "Anvesh"
+ let age = 20
+ let isLoggedIn = false
+ let state = null
+
+ // number => 2^53
+ // bigint
+ // string
+ //boolean => true/false
+ // null => standalone value
+ // undefined
+ // symbol => uniqueness
+
+
+ // object
+ 
+ console.log(typeof null); // object
+ console.log(typeof undefined); // undefined
