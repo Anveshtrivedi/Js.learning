5d1263f290c0d9ba3153959fd4cf9c958bccdb3d
Anvesh Trivedi <trivedianvesh07@gmail.com>
2025-09-23 19:28:58 +0000

    operations discussed

--- 01_Basics/03_ConversionOperation.js
+++ 01_Basics/03_ConversionOperation.js
@@ -1,5 +1,5 @@
 let score = "33abc"
-console.log(typeof score)
+// console.log(typeof score)
 
 let valueInNumber = Number(score)
 // console.log(typeof valueInNumber)
@@ -10,7 +10,7 @@ let valueInNumber = Number(score)
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 =>false
 // "" => false
@@ -18,4 +18,42 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 3
 let StringNumber = String(someNumber)
-console.log(typeof StringNumber);
+// console.log(typeof StringNumber);
+
+// ********************* Operations *******************
+
+let value = 3
+let negValue = -value
+// console.log(negValue); 
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); //  ** indicates power.
+// console.log(2/2); 
+// console.log(2%3); 
+
+let str1 = "Hello"
+let str2 = " Anvesh"
+let str3 = str1 + str2
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3
+num1 = num2 = num3 = 2+2
+
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+
+
+
+
