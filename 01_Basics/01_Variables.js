29af337ffcb9509474d3e327bc96b3a575f79d62
Anvesh Trivedi <trivedianvesh07@gmail.com>
2025-09-23 18:39:50 +0000

    discussion on variables

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accoutEmail = "Anvesh@google.com"
+var accountPassword = "12345"
+accountCity = "Indore"
+let accountState;
+// accountId = 2 // not allowed.
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity, accountState])
+
