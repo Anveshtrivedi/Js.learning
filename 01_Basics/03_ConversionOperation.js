a1ce3557436702d8d5beab205ef2190ee0c77f59
Anvesh Trivedi <trivedianvesh07@gmail.com>
2025-09-23 19:07:41 +0000

    Conversions of data types

---
+++ 01_Basics/03_ConversionOperation.js
@@ -0,0 +1,21 @@
+let score = "33abc"
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber) // Nan => not a number
+
+// "33" => 33
+// "33abc" => Nan
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 =>false
+// "" => false
+//  "Anvesh" => true
+
+let someNumber = 3
+let StringNumber = String(someNumber)
+console.log(typeof StringNumber);
