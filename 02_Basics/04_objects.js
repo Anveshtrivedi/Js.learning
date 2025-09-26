892bc20d1f25ff3fcb2ed188c4fec53f811216b9
Anvesh Trivedi <trivedianvesh07@gmail.com>
2025-09-26 11:39:36 +0000

    Objects part 2

---
+++ 02_Basics/04_objects.js
@@ -0,0 +1,49 @@
+//const tinderUser = new Object() // -> this is the singleton object.
+const tinderUser = {} // -> this is the object literal syntax.
+
+tinderUser.id = '123abc'
+tinderUser.name = 'Anvesh'
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser)
+
+const regularUser = {
+    email: 'some@gmail.com',
+    fullName: {
+        userFullName: {
+            firstName: 'Anvesh',
+            lastName: 'Trivedi'
+        }
+    }
+}
+// console.log(regularUser.fullName.userFullName.firstName) 
+
+const obj1 = { 1: 'a', 2: 'b' }
+const obj2 = {3: 'a' , 4: 'b'}
+//const obj3 = Object.assign({}, obj1, obj2)
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+const users = [
+    {
+        id:1,
+        email: "A@gmail.com"
+    },
+    {
+        id:2,
+        email: "B@gmail.com"
+    },
+    {
+        id:3,
+        email: "c@gmail.com"
+    },
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLogged'));
