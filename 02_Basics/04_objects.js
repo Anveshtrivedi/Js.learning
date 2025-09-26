cdf676fa69742a85e46c3130b4fa24da0510675b
Anvesh Trivedi <trivedianvesh07@gmail.com>
2025-09-26 11:57:36 +0000

    objects and JSON

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -40,10 +40,40 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLogged'));
+// console.log(tinderUser.hasOwnProperty('isLogged'));
+
+
+
+const course = {
+    name: 'js in hindi',
+    price: '999',
+    courseInstructor: 'Hitesh  Choudhary'
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+console.log(instructor);
+
+
+// ******************* JSON APIs **************** //
+
+// {
+//     "name": 'Anvesh',
+//     "coursename": 'Javascript',
+//     "price": '999'
+// }
+
+// {
+//     {},
+//     {},
+//     {}
+// }
+
