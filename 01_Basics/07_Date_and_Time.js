d248e969928f1a08c1bb6c3e664658c27a42e695
Anvesh Trivedi <trivedianvesh07@gmail.com>
2025-09-26 09:54:45 +0000

    date and time

---
+++ 01_Basics/07_Date_and_Time.js
@@ -0,0 +1,34 @@
+let myDate = new Date()
+// console.log(typeof myDate);
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toUTCString());
+ 
+// let myCreatedDate = new Date(2025,11,23)
+// let myCreatedDate = new Date(2025,11,23,5,3)
+let myCreatedDate = new Date("01-09-2025")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+// console.log(newDate.getFullYear());
+
+newDate.toLocaleDateString('default', {
+    weekday:  "long",
+})
+
+// console.log(newDate);
+
+
+
+
