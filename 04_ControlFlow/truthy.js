2ffda930b6c4817e0912281c14c9beacfd3263e5
Anvesh Trivedi <trivedianvesh07@gmail.com>
2025-09-30 02:27:27 +0000

    nullish coalescing operator and ternary operators discussed

--- 04_ControlFlow/truthy.js
+++ 04_ControlFlow/truthy.js
@@ -1,9 +1,9 @@
 const userEmail = "Anvesh@example.com"
 
 if (userEmail) {
-    console.log("You have an email")
+    // console.log("You have an email")
 } else {
-    console.log("You don't have an email")
+    // console.log("You don't have an email")
 }
 
 // 
@@ -20,3 +20,22 @@ if (userEmail) {
 // if (Object.keys(emptyObject).length === 0) {
 //     console.log("Object is empty")
 // }
+
+// Nullish Coalescing Operator (??) : null or undefined
+
+
+let val1;
+ val1 = 5 ?? 10
+val1 = null ?? 10
+
+val1 = undefined ?? 10
+
+val1 = null ?? 10 ?? 20
+
+// console.log(val1)
+
+// Ternary Operator
+// conditon ? true : false
+
+const iceTeaPrice = 50
+ iceTeaPrice >= 80 ? console.log("I will not order IceTea") : console.log("I will order IceTea")
