31101871799370632ec7eb336519ebc160bac7ea
Anvesh Trivedi <trivedianvesh07@gmail.com>
2025-10-06 16:11:58 +0000

    for in loop

---
+++ 05_Iterations/04_iterations.js
@@ -0,0 +1,16 @@
+const myObject = {
+    JS: "JavaScript",
+    cpp: "C++",
+    rb: "Ruby",
+    swift: "Swift by Apple"
+}
+
+for(const key in myObject){
+   // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programmingLanguages = ["JavaScript", "C++", "Ruby", "Swift by Apple"];
+
+for(const key in programmingLanguages){
+    // console.log(programmingLanguages[key]);
+}
