4155ade508bbc85ab48e792535deb58c7d6ec524
Anvesh Trivedi <trivedianvesh07@gmail.com>
2025-10-06 17:15:58 +0000

    more iteration operations

---
+++ 05_Iterations/06_iterations.js
@@ -0,0 +1,36 @@
+// const coding = ["JavaScript", "Python", "Java", "C++", "Ruby"];
+// const values = coding.forEach((item) => {
+//     console.log(item);
+//     return item;
+    
+// })
+
+// console.log(values); 
+
+const myNums = [1,2,3,4,5,6,7,8,9,10];
+
+// const newNums = myNums.filter((num) => {
+//     return num>4
+// })
+
+// const newNums = []
+// myNums.forEach((num) =>{
+//     if(num>4){
+//         newNums.push(num)
+//     }
+// })
+ // console.log(newNums);
+
+
+const myNumbers = [1,2,3,4,5,6,7,8,9,10];
+
+// const newNumbers = myNumbers.map((num) => {
+//     return num+10;
+// })
+
+const newNums = myNumbers
+                .map((num) => num*10)
+                .map((num) => num+1)
+                .filter((num) => num>40)
+
+console.log(newNums);
